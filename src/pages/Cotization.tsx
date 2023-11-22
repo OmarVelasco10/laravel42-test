8f2fdfdca2e461ff9212c27f55aad72587393c14
OmarVelasco10 <ovelasco@ucol.mx>
2023-11-22 11:12:05 -0600

    FIX(cotizationPage): add responsiveness in lg devices

--- src/pages/Cotization.tsx
+++ src/pages/Cotization.tsx
@@ -18,35 +18,41 @@ export const Cotization = () => {
   console.log("result", cotizations);
 
   return (
-    <div className="w-full bg-dark ">
-      <div className="border-2 border-yellow-500 rounded-full flex items-center justify-between">
-        <div className="bg-yellow-500 w-1/3 p-4 rounded-l-full flex justify-between">
-          <p className="font-bold text-xl">{priceSend} €</p>
-          <Link className="text-xl" to="/">
-            Secci
-          </Link>
+    <div className="w-full bg-dark lg:container">
+      <div className="border-2 border-yellow-500 rounded-full flex items-center justify-between lg:flex-no-wrap lg:rounded-full lg:overflow-hidden lg:max-w-[80%] xl:max-w-[90%] lg:flex-col xl:flex-row lg:items-baseline">
+        <div className="flex lg:w-2/3">
+          <div className="bg-yellow-500 lg:w-[40%] lg:rounded-t-lg xl:rounded-t-none lg:rounded-l-full lg:rounded-bl-none lg:rounded-tr-none xl:rounded-l-none xl:rounded-r-none w-1/3 p-4 rounded-l-full flex justify-between">
+            <p className="font-bold text-xl">{priceSend} €</p>
+            <Link className="text-xl" to="/">
+              Secci
+            </Link>
+          </div>
+          <div className="lg:w-2/3 w-full flex mt-0 items-center justify-center p-2">
+            <p className="text-lg">
+              <span className="font-bold">{cotizations[0].imp_rata} € </span>
+              per
+              <span className="font-bold"> {cotizations[0].rate} </span>
+              mesi
+              <span className="font-bold"> TAEG {cotizations[0].taeg}</span>
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="text-lg">
-            <span className="font-bold">{cotizations[0].imp_rata} € </span>
-            per
-            <span className="font-bold"> {cotizations[0].rate} </span>
-            mesi
-            <span className="font-bold"> TAEG {cotizations[0].taeg}</span>
-          </p>
+        <div className="w-full lg:w-auto lg:ml-auto lg:text-right">
+          <button className="bg-yellow-500 p-2 px-16 rounded-full text-lg font-bold sm:mt-0.5 sm:mr-0.5 shadow-xl lg:shadow-none hover:bg-yellow-400 xl:h-14">
+            Continua
+          </button>
         </div>
-        <button className="bg-yellow-500 p-2 px-16 rounded-full text-lg font-bold sm:mt-0.5 sm:mr-0.5 shadow-xl lg:shadow-none hover:bg-yellow-400 xl:h-14">
-          Continua
-        </button>
       </div>
       <p className="my-8">Altre proposte di prestito personale</p>
       <div className="flex flex-col gap-4">
         {cotizations.map(
           (item: any, index: any) =>
             index !== 0 && (
-              <div className="border-2 border-black rounded-full flex items-center justify-between">
+              <div className="border-2 border-black rounded-full flex items-center justify-between lg:max-w-[80%]">
                 <div className="w-1/3 p-4 border-r-2 border-black-500 rounded-l-full flex justify-between items-center">
-                  <strong className="xl:ml-10 0  whitespace-no-wrap text-xl">{priceSend} €</strong>
+                  <strong className="xl:ml-10 0  whitespace-no-wrap text-xl">
+                    {priceSend} €
+                  </strong>
                   <Link className="text-xl" to="/">
                     Secci
                   </Link>
